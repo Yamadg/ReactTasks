d9fc9508581f3a1264e20283341c9e0150ddba4c
Yama <goodyamaki.@mail.ru>
2022-08-17 14:58:19 +0300

    1

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './index.scss';
+import React from "react";
+
+function App() {
+    const [counter, setCounter] = React.useState(0);
+
+    const onClickPlus = () =>{
+        setCounter(counter +1)
+    }
+    const onClickMinus = () => {
+      setCounter(counter -1)
+    }
+
+  return (
+    <div className="App">
+      <div>
+        <h2>Счетчик:</h2>
+        <h1>{counter}</h1>
+        <button onClick={onClickMinus} className="minus">- Минус</button>
+        <button onClick={onClickPlus} className="plus">Плюс +</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
